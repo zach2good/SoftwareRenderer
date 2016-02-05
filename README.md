5606ae75da3575f884e8ddffa68b9f3274f1bb24
Zach Toogood <zrtoogood@gmail.com>
2016-02-05 15:59:16 +0000

    Lesson 3 part 2

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 SDL2 Software Renderer 
 following along with https://github.com/ssloy/tinyrenderer/wiki
 
-![image](https://raw.githubusercontent.com/zach2good/SoftwareRenderer/master/img/002.png "ScreenShot2")
+![image](https://raw.githubusercontent.com/zach2good/SoftwareRenderer/master/img/003.png "ScreenShot3")
--- main.cpp
+++ main.cpp
@@ -11,12 +11,15 @@
 #include "model.h"
 #include "geometry.h"
 
-const int width = 1024;
-const int height = 768;
-const int depth = 255;
-Model* model = NULL;
-int* zbuffer = NULL;
+
+const int width  = 800;
+const int height = 800;
+const int depth  = 255;
+
+Model *model = NULL;
+int *zbuffer = NULL;
 Vec3f light_dir(0,0,-1);
+Vec3f camera(0,0,3);
 
 struct sdl_deleter
 {
@@ -83,47 +86,73 @@ void line(SDL_Renderer* renderer, int x0, int y0, int x1, int y1)
     }
 }
 
-void triangle(SDL_Renderer* renderer, Vec3i t0, Vec3i t1, Vec3i t2, int r, int g, int b, int* zbuffer)
+Vec3f m2v(Matrix m) 
 {
-    if (t0.y == t1.y && t0.y == t2.y) return; 
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
+}
 
-    if (t0.y > t1.y) std::swap(t0, t1);
-    if (t0.y > t2.y) std::swap(t0, t2);
-    if (t1.y > t2.y) std::swap(t1, t2);
+Matrix v2m(Vec3f v) 
+{
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
 
-    int total_height = t2.y - t0.y;
+Matrix viewport(int x, int y, int w, int h) 
+{
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = depth/2.f;
 
-    for (int i = 0; i < total_height; i++)
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = depth/2.f;
+    return m;
+}
+
+void triangle(SDL_Renderer* renderer, Vec3i t0, Vec3i t1, Vec3i t2, Vec2i uv0, Vec2i uv1, Vec2i uv2, float intensity, int *zbuffer) 
+{
+    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
+
+    if (t0.y>t1.y) { std::swap(t0, t1); std::swap(uv0, uv1); }
+    if (t0.y>t2.y) { std::swap(t0, t2); std::swap(uv0, uv2); }
+    if (t1.y>t2.y) { std::swap(t1, t2); std::swap(uv1, uv2); }
+
+    int total_height = t2.y-t0.y;
+
+    for (int i=0; i<total_height; i++) 
     {
-        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
-        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
-        float alpha = (float)i / total_height;
-        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
 
-        Vec3i A = t0 + (t2 - t0) * alpha;
-        Vec3i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        Vec3i A   =               t0  + Vec3f(t2-t0  )*alpha;
+        Vec3i B   = second_half ? t1  + Vec3f(t2-t1  )*beta : t0  + Vec3f(t1-t0  )*beta;
+        Vec2i uvA =               uv0 +      (uv2-uv0)*alpha;
+        Vec2i uvB = second_half ? uv1 +      (uv2-uv1)*beta : uv0 +      (uv1-uv0)*beta;
 
-        if (A.x > B.x) std::swap(A, B);
+        if (A.x>B.x) { std::swap(A, B); std::swap(uvA, uvB); }
 
-        for (int j = A.x; j <= B.x; j++)
+        for (int j=A.x; j<=B.x; j++) 
         {
-            float phi = B.x == A.x ? 1. : (float)(j - A.x) / (float)(B.x - A.x);
-            Vec3i P = A + (B - A) * phi;
-            P.x = j; P.y = t0.y + i; 
-            int idx = j + (t0.y + i) * width;
+            float phi = B.x==A.x ? 1. : (float)(j-A.x)/(float)(B.x-A.x);
+            Vec3i   P = Vec3f(A) + Vec3f(B-A)*phi;
+            Vec2i uvP =     uvA +   (uvB-uvA)*phi;
+            int idx = P.x+P.y*width;
 
-            if (zbuffer[idx] < P.z)
+            if (zbuffer[idx]<P.z) 
             {
                 zbuffer[idx] = P.z;
-                pixel(renderer, P.x, P.y, r, g, b);
+                TGAColor color = model->diffuse(uvP);
+               pixel(renderer, P.x, P.y, color.r*intensity, color.g*intensity, color.b*intensity);
             }
         }
     }
-}
-
-Vec3f world2screen(Vec3f v)
-{
-    return Vec3f(int((v.x + 1.) * width / 2.+ .5), int((v.y + 1.) * height / 2.+ .5), v.z);
 }
 
 
@@ -186,27 +215,37 @@ int main(int argc, char** argv)
             zbuffer[i] = std::numeric_limits < int >::min();
         }
 
-        for (int i = 0; i < model->nfaces(); i++)
+         // draw the model
+        Matrix Projection = Matrix::identity(4);
+        Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
+        Projection[3][2] = -1.f/camera.z;
+
+        for (int i=0; i<model->nfaces(); i++) 
         {
             std::vector<int> face = model->face(i);
             Vec3i screen_coords[3];
             Vec3f world_coords[3];
 
-            for (int j = 0; j < 3; j++)
+            for (int j=0; j<3; j++) 
             {
                 Vec3f v = model->vert(face[j]);
-                screen_coords[j] = Vec3i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2., (v.z + 1.) * depth / 2.);
-                world_coords[j] = v;
+                screen_coords[j] =  m2v(ViewPort*Projection*v2m(v));
+                world_coords[j]  = v;
             }
 
-            Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
-
+            Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
             n.normalize();
-            float intensity = n * light_dir;
+            float intensity = n*light_dir;
 
-            if (intensity > 0)
+            if (intensity>0) 
             {
-                triangle(renderer.get(), screen_coords[0], screen_coords[1], screen_coords[2], intensity * 255, intensity * 255, intensity * 255, zbuffer);
+                Vec2i uv[3];
+                for (int k=0; k<3; k++) 
+                {
+                    uv[k] = model->uv(i, k);
+                }
+
+                triangle(renderer.get(), screen_coords[0], screen_coords[1], screen_coords[2], uv[0], uv[1], uv[2], intensity, zbuffer);
             }
         }
 
